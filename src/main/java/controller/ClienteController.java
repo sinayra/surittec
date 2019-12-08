1f35685954a8de2ee769520bda62c8bedf29b32f
sinayra <nayra@hotmail.co.jp>
2019-12-08 12:33:09 -0200

    Rota cliente que retorna json com todos os clientes

---
+++ src/main/java/controller/ClienteController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import model.dao.ClienteDAO;
+import model.entity.Cliente;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteController {
+
+	private ClienteDAO dao;
+	
+
+	@RequestMapping("/")
+	public String listCliente(Model map) throws Exception {
+		dao = ClienteDAO.getInstance();
+		List<Cliente> clientes;
+		
+		try {
+			clientes = dao.read();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new Exception(e);
+		}
+		
+		map.addAttribute("clientes", clientes);
+		
+		return "jsonTemplate";
+	}
+	
+}
