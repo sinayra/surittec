52abf74d0f1951a07a83ccc14759ded496e8027c
sinayra <nayra@hotmail.co.jp>
2019-12-08 02:22:33 -0200

    Entidade Cliente simples

---
+++ src/main/java/entity/Cliente.java
@@ -0,0 +1,53 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+  
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "nome", length=100)
+    private String nome;
+	
+	@Column(name = "cpf", length=11)
+    private String cpf;
+	
+	public Cliente(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
