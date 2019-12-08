10b5c9fd15a047d437bb82f9af6841a435a879e4
sinayra <nayra@hotmail.co.jp>
2019-12-08 19:00:07 -0300

    Rota de adicionar cliente

--- src/main/java/controller/ClienteController.java
+++ src/main/java/controller/ClienteController.java
@@ -2,8 +2,13 @@ package controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import model.dao.ClienteDAO;
@@ -16,7 +21,7 @@ public class ClienteController {
 	private ClienteDAO dao;
 	
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String ClienteList(Model map) throws Exception {
 		dao = ClienteDAO.getInstance();
 		List<Cliente> clientes;
@@ -31,6 +36,20 @@ public class ClienteController {
 		map.addAttribute("clientes", clientes);
 		
 		return "jsonTemplate";
+	}
+	
+	@PostMapping(value = "/addcliente", consumes = "application/json")
+	public ResponseEntity<String> AddCliente(@RequestBody Cliente cliente) throws Exception {
+		dao = ClienteDAO.getInstance();
+		
+		try {
+			dao.create(cliente);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new Exception(e);
+		}
+				
+		return new ResponseEntity<String>("cliente adicionado", HttpStatus.OK);
 	}
 	
 }
